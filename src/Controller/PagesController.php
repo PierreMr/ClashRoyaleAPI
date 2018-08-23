149b5c2a20ef5712aed5787cf0e4088bcfb54d1d
PierreMr <mauerpierre@gmail.com>
2018-08-23 10:19:57 +0200

    :beers::beers::beers:

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -14,7 +14,22 @@ class PagesController extends AbstractController
      */
     public function home()
     {
-        return $this->render('pages/index.html.twig');
+        $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, 'https://api.chucknorris.io/jokes/random');
+        curl_setopt($ch, CURLOPT_URL, 'https://api.punkapi.com/v2/beers/random');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json; charset=utf-8')); // Assuming you're requesting JSON
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($ch);
+
+        // If using JSON...
+        $api = json_decode($response);
+
+        // var_dump($api);
+
+        return $this->render('pages/index.html.twig', [
+            'api' => $api
+        ]);
     }
 
     /**
@@ -24,7 +39,7 @@ class PagesController extends AbstractController
     {
     	$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, 'https://api.royaleapi.com/tournaments/open');
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUyOCwiaWRlbiI6IjI1ODU0ODAzNTA3MDQ1OTkwNSIsIm1kIjp7fSwidHMiOjE1MzQ4MzU5Mzg4MDJ9.36oCS7ixAoePvtpCEhjaGip1MfWckwWNcp_WpDjz1MU')); // Assuming you're requesting JSON
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-type: application/json', 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUyOCwiaWRlbiI6IjI1ODU0ODAzNTA3MDQ1OTkwNSIsIm1kIjp7fSwidHMiOjE1MzQ4MzU5Mzg4MDJ9.36oCS7ixAoePvtpCEhjaGip1MfWckwWNcp_WpDjz1MU']); // Assuming you're requesting JSON
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 		$response = curl_exec($ch);
